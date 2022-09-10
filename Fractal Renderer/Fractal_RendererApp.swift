38531d713a7f40fce28588ca2f2021c756354ee5
Jake <jakedves@gmail.com>
2022-09-10 22:18:40 +0100

    Initial Commit

---
+++ Fractal Renderer/Fractal_RendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fractal_RendererApp.swift
+//  Fractal Renderer
+//
+//  Created by Jake Davies on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fractal_RendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
